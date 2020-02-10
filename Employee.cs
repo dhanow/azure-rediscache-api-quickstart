883c73632d2437149efed354dcd3c0d3a744f470
dhanow <dhanow@yahoo.com>
2020-02-09 22:24:12 -0600

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace AzureRedisCacheApi
+{
+    class Employee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Employee(string EmployeeId, string Name, int Age)
+        {
+            this.Id = EmployeeId;
+            this.Name = Name;
+            this.Age = Age;
+        }
+    }
+}
